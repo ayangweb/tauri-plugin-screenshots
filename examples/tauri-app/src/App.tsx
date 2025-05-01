38d3a592ffe285b111e7091ffb52f14910701760
ayang <473033518@qq.com>
2025-05-01 10:49:49 +0800

    feat: the window adds the fields of `title` and `appName`

--- examples/tauri-app/src/App.tsx
+++ examples/tauri-app/src/App.tsx
@@ -9,8 +9,13 @@ import {
 } from "tauri-plugin-screenshots-api";
 import { Button, Divider, List, Image, Spin, message } from "antd";
 import { convertFileSrc } from "@tauri-apps/api/core";
+import { getName } from "@tauri-apps/api/app";
+import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
 
 interface State {
+  appName: string;
+  windowTitle: string;
+  currentWindow?: ScreenshotableWindow;
   screenshotableWindows: ScreenshotableWindow[];
   screenshotableMonitors: ScreenshotableMonitor[];
   windowScreenshots: Record<number, string>;
@@ -19,6 +24,8 @@ interface State {
 
 const App = () => {
   const state = useReactive<State>({
+    appName: "",
+    windowTitle: "",
     screenshotableWindows: [],
     screenshotableMonitors: [],
     windowScreenshots: {},
@@ -28,7 +35,22 @@ const App = () => {
   const [loading, { toggle }] = useBoolean();
 
   useMount(async () => {
-    state.screenshotableWindows = await getScreenshotableWindows();
+    const appWindow = getCurrentWebviewWindow();
+
+    state.appName = await getName();
+    state.windowTitle = await appWindow.title();
+
+    const screenshotableWindows = await getScreenshotableWindows();
+
+    for (const window of screenshotableWindows) {
+      const { appName, title } = window;
+
+      if (appName === state.appName && title === state.windowTitle) {
+        state.currentWindow = window;
+      } else {
+        state.screenshotableWindows.push(window);
+      }
+    }
 
     state.screenshotableMonitors = await getScreenshotableMonitors();
   });
@@ -36,6 +58,48 @@ const App = () => {
   return (
     <>
       <Spin fullscreen spinning={loading} />
+
+      <Divider orientation="left">Current Window</Divider>
+
+      <List
+        bordered
+        dataSource={state.currentWindow ? [state.currentWindow] : []}
+        renderItem={(item) => {
+          const { id, name } = item;
+
+          return (
+            <List.Item
+              key={id}
+              actions={[
+                <Button
+                  onClick={async () => {
+                    try {
+                      toggle();
+
+                      const url = await getWindowScreenshot(id);
+
+                      state.windowScreenshots[id] = convertFileSrc(url);
+                    } catch (error) {
+                      message.error(String(error));
+                    } finally {
+                      toggle();
+                    }
+                  }}
+                >
+                  Screenshot
+                </Button>,
+                <Image
+                  width={50}
+                  height={50}
+                  src={state.windowScreenshots[id]}
+                />,
+              ]}
+            >
+              {name}
+            </List.Item>
+          );
+        }}
+      />
 
       <Divider orientation="left">Screenshotable Windows</Divider>
 
